11a97ebac87a7a95bfcd4e79c81bc9309781711d
Terry Hunt <terry.hunt@sofwerx.org>
2025-04-12 06:58:43 -0400

    Implement security headers middleware to enhance application security

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,14 +4,32 @@ export default [
   {
     name: "strapi::security",
     config: {
-      contentSecurityPolicy: false, // We handle this in our custom middleware
-      xssFilter: false, // We handle this in our custom middleware
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "default-src": ["'self'"],
+          "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+          "style-src": ["'self'", "'unsafe-inline'"],
+          "img-src": ["'self'", "data:", "blob:"],
+          "font-src": ["'self'"],
+          "connect-src": ["'self'"],
+          "media-src": ["'self'"],
+          "object-src": ["'none'"],
+          "frame-src": ["'self'"],
+          "base-uri": ["'self'"],
+          "form-action": ["'self'"],
+        },
+      },
+      xssFilter: true,
+      frameguard: {
+        action: "sameorigin",
+      },
     },
   },
   {
     name: "strapi::cors",
     config: {
-      origin: ["http://localhost:3000", "https://yourlawfirmwebsite.com"], // Add your frontend domains
+      origin: ["http://localhost:3000", "https://yourlawfirmwebsite.com"],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
       headers: ["Content-Type", "Authorization", "Origin", "Accept"],
       keepHeaderOnError: true,
@@ -24,8 +42,4 @@ export default [
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
-  {
-    name: "api::middlewares.security-headers",
-    config: {},
-  },
 ];
